9cf26b4c502cf27ca8c0e85824939de2aacd520c
Tesnim19 <tesnimabdi67@gmail.com>
2023-06-15 06:19:08 +0300

    few edits left

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
     content: ['./*.html'],
     darkMode: 'class', // or 'media' or 'class'
     theme: {
+        fontFamily: {
+        'Roboto': ['sans-serif'],
+        'Grotesk': ['sans-serif'],
+        'mono': ["SF Mono","Fira Code","Fira Mono","Roboto Mono",'monospace'],
+        "sans" : ['Calibre','Inter',"San Francisco","SF Pro Text",'-apple-system','system-ui','sans-serif']
+        
+        },
+        fontWeight: {
+            thin: '100',
+            hairline: '100',
+            extralight: '200',
+            light: '300',
+            normal: '400',
+            medium: '500',
+            semibold: '600',
+            bold: '700',
+            extrabold: '800',
+            'extra-bold': '800',
+            black: '900',
+          },
         extend: {
+            backdropBlur: {
+                'md': 'blur(20px)',
+              },
             colors: {
                 //add your own color
                 //https://tailwindcss.com/docs/customizing-colors
@@ -11,10 +34,16 @@ module.exports = {
             container: {
                 center: true,
             },
+            backgroundImage: {
+                'grayscale' :"linear-gradient(45deg, #243949 10%, #517fa4 100%)",
+                'bluescale': "linear-gradient(45.4deg,  rgba(7,7,9,1) 16.5%, rgba(27,24,113,1) 130.2% )"
+            }
         },
     },
     variants: {
-        extend: {},
+        extend: {
+            backdropBlur: ['responsive'],
+        },
     },
     plugins: [],
 }
